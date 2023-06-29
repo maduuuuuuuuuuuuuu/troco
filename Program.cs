2d9bc0ef848aa1ced42a87b15c73400f567b0647
madu <maduamaral2006@gmail.com>
2023-06-29 13:27:12 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Double valordacompra;
+Double valorpago;
+Double troco;
+Console.WriteLine("digite o valor da sua compra");
+valordacompra = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("digite o valor pago");
+valorpago = Convert.ToDouble(Console.ReadLine());
+troco = valorpago - valordacompra;
+Console.WriteLine($"o seu troco é de R${ troco } ");
+
+
